60c432e3ad552b2cdcddd8b6603cce2f78650eb3
Andrew Calderwood <pandapops3665@gmail.com>
2022-03-03 20:55:46 +0000

    fixed issue

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,21 +1,17 @@
-
-
-
 const NavBar = () => {
-return (<nav className="navbar is-primary" role="navigation" aria-label="main navigation">
-<div className="navbar-brand">
-  <a className="navbar-item" href="https://bulma.io">
-    <h2>Sherlock</h2>
-  </a>
+  return (
+    <nav
+      className="navbar is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <h2>Sherlock</h2>
+        </a>
+      </div>
+    </nav>
+  );
+};
 
-  <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-    <span aria-hidden="true"></span>
-    <span aria-hidden="true"></span>
-    <span aria-hidden="true"></span>
-  </a>
-</div>
-</nav>)
-}
-
-
-export {NavBar}
+export { NavBar };
