08a3a03b984b503df1e7313980ec6c92cfa3b7d9
Andrew Calderwood <pandapops3665@gmail.com>
2022-03-04 00:06:53 +0000

    added vercel branding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,34 @@
+import Container from "./Container";
+
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="content has-text-centered">
-        <p>
-          <strong>Sherlock</strong> by{" "}
-          <a href="https://github.com/Acalderwood93">Andrew Calderwood </a>.
-        </p>
-        <p>
-          <strong>
-            Powered by{" "}
-            <a href="https://www.apollographql.com/">Apollo GraphQL,</a>
-          </strong>{" "}
-          API Available{" "}
-          <a href="https://api.sherlock.pandapops.me">
-            <strong>here</strong>
+      <Container>
+        <div className="content has-text-centered">
+          <p>
+            <strong>Sherlock</strong> by{" "}
+            <a href="https://github.com/Acalderwood93">Andrew Calderwood </a>.
+          </p>
+          <p>
+            <strong>
+              Powered by{" "}
+              <a href="https://www.apollographql.com/">Apollo GraphQL,</a>
+            </strong>{" "}
+            API Available{" "}
+            <a href="https://api.sherlock.pandapops.me">
+              <strong>here</strong>
+            </a>
+            .
+          </p>
+          <a href="https://vercel.com/">
+            <img
+              src="/vercel-logotype-dark.svg"
+              style={{ width: "5em" }}
+              alt="vercel logo"
+            ></img>
           </a>
-          .
-        </p>
-      </div>
+        </div>
+      </Container>
     </footer>
   );
 };
