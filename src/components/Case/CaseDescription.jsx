23c7fe7c0492ee42c9e0f2aa9eaa897be1d06344
ACalderwood93 <andrew.calderwood93@gmail.com>
2022-03-25 10:58:32 +0000

    changed from text area to div with pre wrap

--- src/components/Case/CaseDescription.jsx
+++ src/components/Case/CaseDescription.jsx
@@ -2,7 +2,7 @@ const CaseDescription = ({ description }) => {
   return (
     <section class="section">
       <h1 class="title">Description</h1>
-      <textarea className="case-description" readOnly>{description}</textarea>
+      <div className="case-description">{description}</div>
       
       
     </section>
