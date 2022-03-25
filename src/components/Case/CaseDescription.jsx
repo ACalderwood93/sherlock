7846d0d323ec2a2afcb3eb568299c10a03e86dc4
ACalderwood93 <andrew.calderwood93@gmail.com>
2022-03-25 10:52:03 +0000

    changed case description to text area to allow for correct text formatting

--- src/components/Case/CaseDescription.jsx
+++ src/components/Case/CaseDescription.jsx
@@ -2,7 +2,9 @@ const CaseDescription = ({ description }) => {
   return (
     <section class="section">
       <h1 class="title">Description</h1>
-      <h2 class="subtitle">{description}</h2>
+      <textarea className="case-description" readOnly>{description}</textarea>
+      
+      
     </section>
   );
 };
